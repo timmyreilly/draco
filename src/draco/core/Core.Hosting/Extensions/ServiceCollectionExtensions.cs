c427bacfa8d99e53c575b58f17b6ba50f011f2ee
Casey Watson <cawatson@microsoft.com>
2020-04-14 16:29:21 -0500

    Added comments to Core.Hosting...

--- src/draco/core/Core.Hosting/Extensions/ServiceCollectionExtensions.cs
+++ src/draco/core/Core.Hosting/Extensions/ServiceCollectionExtensions.cs
@@ -8,8 +8,18 @@ using System;
 
 namespace Draco.Core.Hosting.Extensions
 {
+    /// <summary>
+    /// Extension methods needed to work with service collections and modules.
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Configures all the services defined in [TModule].
+        /// </summary>
+        /// <param name="services">The hosting application's service collection</param>
+        /// <param name="configuration">The hosting application's configuration</param>
+        /// <typeparam name="TModule">The type of module to configure</typeparam>
+        /// <returns></returns>
         public static IServiceCollection ConfigureServices<TModule>(this IServiceCollection services, IConfiguration configuration)
             where TModule : IServiceModule, new()
         {
@@ -22,6 +32,8 @@ namespace Draco.Core.Hosting.Extensions
             {
                 throw new ArgumentNullException(nameof(configuration));
             }
+
+            // Create a service module [TModule] and register its services with the hosting application's service collection.
 
             new TModule().ConfigureServices(services, configuration);
 
--- src/draco/core/Core.Hosting/Interfaces/IServiceModule.cs
+++ src/draco/core/Core.Hosting/Interfaces/IServiceModule.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Draco.Core.Hosting.Interfaces
 {
+    /// <summary>
+    /// Defines a mechanism for registering a logical grouping of service(s) with the
+    /// hosting application's service collection.
+    /// </summary>
     public interface IServiceModule
     {
         void ConfigureServices(IServiceCollection services, IConfiguration configuration);
